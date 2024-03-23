42adde9468e9a9435d97e0dd0d282b5405c43850
Andreas Conrad <mail@anconrad.de>
2024-03-23 15:08:11 +0100

    symel my lenk

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,5 +4,27 @@
 
 set -euo pipefail
 
-sudo ln -s .dotfiles/.zshrc /root/.zshrc
-sudo ln -s .dotfiles/.oh-my-zsh-custom /root/.oh-my-zsh-custom
+SCRIPT_DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
+
+move_and_symlink() {
+    local source="$1"
+    local target="$2"
+    local backup="${target}.bak"
+    
+    if [ -e "$target" ] && [ ! -L "$target" ]; then
+        echo "$target exists and is not a symlink. Moving to $backup"
+        sudo mv "$target" "$backup"
+    fi
+
+    sudo ln -snf "$source" "$target"
+}
+
+move_and_symlink "$SCRIPT_DIR/.zshrc" /root/.zshrc
+move_and_symlink "$SCRIPT_DIR/.zshrc" "$HOME/.zshrc"
+
+move_and_symlink "$SCRIPT_DIR/.oh-my-zsh-custom" /root/.oh-my-zsh-custom
+move_and_symlink "$SCRIPT_DIR/.oh-my-zsh-custom" "$HOME/.oh-my-zsh-custom"
+
+move_and_symlink "$HOME/.oh-my-zsh" /root/.oh-my-zsh
+
+move_and_symlink "$SCRIPT_DIR/.gitconfig" "$HOME/.gitconfig"

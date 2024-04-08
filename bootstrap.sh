d442a06555693372c4d6f2c141f3ae1796165f9e
Andreas Conrad <mail@anconrad.de>
2024-04-08 09:44:12 +0200

    scripts update

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,7 +10,7 @@ move_and_symlink() {
     local source="$1"
     local target="$2"
     local backup="${target}.bak"
-    
+
     if [ -e "$target" ] && [ ! -L "$target" ]; then
         echo "$target exists and is not a symlink. Moving to $backup"
         sudo mv "$target" "$backup"
@@ -19,6 +19,8 @@ move_and_symlink() {
     sudo ln -snf "$source" "$target"
 }
 
+
+# zsh
 move_and_symlink "$SCRIPT_DIR/.zshrc" /root/.zshrc
 move_and_symlink "$SCRIPT_DIR/.zshrc" "$HOME/.zshrc"
 
@@ -27,4 +29,13 @@ move_and_symlink "$SCRIPT_DIR/.oh-my-zsh-custom" "$HOME/.oh-my-zsh-custom"
 
 move_and_symlink "$HOME/.oh-my-zsh" /root/.oh-my-zsh
 
+
+# git
 move_and_symlink "$SCRIPT_DIR/.gitconfig" "$HOME/.gitconfig"
+
+
+# custom scripts
+for script in "$SCRIPT_DIR/scripts"/*; do
+    script_name=$(basename "$script")
+    move_and_symlink "$script" "/usr/local/bin/$script_name"
+done
